7299dc5470576798733957b0429f48941ee545d6
iiTONELOC <anthonytropeano@me.com>
2024-07-03 20:49:57 -0400

    Created new Next.js project and installed Jest for testing

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  ignorePatterns: ['**/node_modules/**']
+};
